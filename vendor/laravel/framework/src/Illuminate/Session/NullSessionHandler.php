8e577212242b8c8c54609b95cc8a05b03467b04d
Jarvis <jarvis.okay@gmail.com>
2021-08-02 00:03:00 +0800

    Framework v8.52.0

---
+++ vendor/laravel/framework/src/Illuminate/Session/NullSessionHandler.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Illuminate\Session;
+
+use ReturnTypeWillChange;
+use SessionHandlerInterface;
+
+class NullSessionHandler implements SessionHandlerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    #[ReturnTypeWillChange]
+    public function open($savePath, $sessionName)
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    #[ReturnTypeWillChange]
+    public function close()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    #[ReturnTypeWillChange]
+    public function read($sessionId)
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    #[ReturnTypeWillChange]
+    public function write($sessionId, $data)
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    #[ReturnTypeWillChange]
+    public function destroy($sessionId)
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    #[ReturnTypeWillChange]
+    public function gc($lifetime)
+    {
+        return true;
+    }
+}
