930abff38353ce0f6924b0efb59197d5fe4ae533
Jarvis <jarvis.okay@gmail.com>
2021-11-07 20:12:06 +0800

    chore: edit connects info

--- database/seeders/ConfigsTableSeeder.php
+++ database/seeders/ConfigsTableSeeder.php
@@ -357,7 +357,7 @@ class ConfigsTableSeeder extends Seeder
             26 => [
                 'id' => 27,
                 'item_key' => 'connects',
-                'item_value' => '[{"id":"1","name":"Other"},{"id":"2","name":"GitHub"},{"id":"3","name":"Google"},{"id":"4","name":"Facebook"},{"id":"5","name":"Twitter"},{"id":"6","name":"Apple"},{"id":"7","name":"Gitee"},{"id":"8","name":"WeChat Open Platform (UnionID)"},{"id":"9","name":"WeChat Official Accounts Platform"},{"id":"10","name":"WeChat Mini Program"},{"id":"11","name":"WeChat Mobile Application"},{"id":"12","name":"WeChat Website Application"},{"id":"13","name":"Tencent QQ"},{"id":"14","name":"Weibo"},{"id":"15","name":"Alipay"},{"id":"16","name":"ByteDance MicroApp"},{"id":"17","name":"Baidu SmartProgram"},{"id":"18","name":"360 MiniApp"}]',
+                'item_value' => '[{"id":"1","name":"Other"},{"id":"2","name":"GitHub"},{"id":"3","name":"Google"},{"id":"4","name":"Facebook"},{"id":"5","name":"Twitter"},{"id":"6","name":"Apple"},{"id":"7","name":"Gitee"},{"id":"8","name":"WeChat Open Platform (UnionID)"},{"id":"9","name":"WeChat Official Accounts Platform"},{"id":"10","name":"WeChat Mini Program"},{"id":"11","name":"WeChat Mobile Application"},{"id":"12","name":"WeChat Website Application"},{"id":"13","name":"Tencent QQ"},{"id":"14","name":"Weibo"},{"id":"15","name":"Alipay"},{"id":"16","name":"ByteDance MicroApp"},{"id":"17","name":"Baidu SmartProgram"}]',
                 'item_type' => 'array',
                 'item_tag' => 'commons',
                 'is_multilingual' => 0,
