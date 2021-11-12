fdc142002a4e8f65d3644e9995787b3538fa900e
Jarvis Tang <jarvis-tang@users.noreply.github.com>
2021-11-12 07:08:06 +0000

    Apply fixes from StyleCI

--- app/Console/Commands/UpgradeCommand.php
+++ app/Console/Commands/UpgradeCommand.php
@@ -9,13 +9,13 @@
 namespace App\Console\Commands;
 
 use App\Helpers\FileHelper;
+use App\Helpers\Http;
 use App\Http\Center\Helper\InstallHelper;
 use App\Http\FresnsPanel\FsService;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
-use App\Helpers\Http;
 
 class UpgradeCommand extends Command
 {
@@ -55,29 +55,33 @@ class UpgradeCommand extends Command
         $this->line('step1: check version');
         $versionInfo = FsService::getVersionInfo();
         if (! $versionInfo['canUpgrade']) {
-            $this->error('there is no new version');exit;
+            $this->error('there is no new version');
+            exit;
         }
         //Step 2
         $this->line('step2: download package');
-        $downloadDir  = storage_path('app/upgrade');
+        $downloadDir = storage_path('app/upgrade');
         FileHelper::assetDir($downloadDir);
         $downloadFile = $downloadDir.'/upgrade_'.md5(time()).'.zip';
-        $result = Http::get($versionInfo['upgradePackage'],function ($http) use ($downloadFile) {
+        $result = Http::get($versionInfo['upgradePackage'], function ($http) use ($downloadFile) {
             $http->timeout(600);
             $http->toFile($downloadFile);
         });
         if ($result->code != 200) {
-            $this->error("download package fail");exit;
+            $this->error('download package fail');
+            exit;
         }
         $fileSize = File::size($downloadFile);
         if ($fileSize < 10) {
-            $this->error('download package fail');exit;
+            $this->error('download package fail');
+            exit;
         }
         //Step 3
         $this->line('step3: unzip package');
         $status = FileHelper::unzip($downloadFile, $downloadDir);
         if ($status == false) {
-            $this->error('unzip package fail');exit;
+            $this->error('unzip package fail');
+            exit;
         }
         //Step 4
         $this->line('step4: copy file');
