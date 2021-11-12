912e4b10cea53f8fd4834103986bf5b8defe0f1a
Jarvis <jarvis.okay@gmail.com>
2021-11-12 15:07:54 +0800

    chore: upgrade command

--- app/Console/Commands/UpgradeCommand.php
+++ app/Console/Commands/UpgradeCommand.php
@@ -15,6 +15,7 @@ use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
+use App\Helpers\Http;
 
 class UpgradeCommand extends Command
 {
@@ -49,28 +50,37 @@ class UpgradeCommand extends Command
      */
     public function handle()
     {
-        $this->info('upgrade start');
+        $this->info('Fresns Upgrade Start');
+        //Step 1
         $this->line('step1: check version');
         $versionInfo = FsService::getVersionInfo();
         if (! $versionInfo['canUpgrade']) {
-            $this->error('there is no new version');
+            $this->error('there is no new version');exit;
         }
+        //Step 2
         $this->line('step2: download package');
-        $downloadDir = storage_path('app/upgrade/v_'.$versionInfo['upgradeVersion'].'/');
-        $filename = date('YmdH').'.zip';
-        $downloadFile = $downloadDir.$filename;
-        FileHelper::assetDir($downloadFile);
-        FsService::downFile($versionInfo['upgradePackage'], $downloadFile);
+        $downloadDir  = storage_path('app/upgrade');
+        FileHelper::assetDir($downloadDir);
+        $downloadFile = $downloadDir.'/upgrade_'.md5(time()).'.zip';
+        $result = Http::get($versionInfo['upgradePackage'],function ($http) use ($downloadFile) {
+            $http->timeout(600);
+            $http->toFile($downloadFile);
+        });
+        if ($result->code != 200) {
+            $this->error("download package fail");exit;
+        }
         $fileSize = File::size($downloadFile);
         if ($fileSize < 10) {
-            $this->error('download package fail');
+            $this->error('download package fail');exit;
         }
+        //Step 3
         $this->line('step3: unzip package');
         $status = FileHelper::unzip($downloadFile, $downloadDir);
         if ($status == false) {
-            $this->error('unzip package fail');
+            $this->error('unzip package fail');exit;
         }
-        $this->line('step4: copy document');
+        //Step 4
+        $this->line('step4: copy file');
         $coverPath = ['Base', 'Console', 'Exceptions', 'Helpers', 'Http', 'Listeners', 'Providers', 'Traits', 'static', 'views', 'lang', 'migrations', 'seeders'];
         foreach ($coverPath as $subDir) {
             if (in_array($subDir, ['Base', 'Console', 'Exceptions', 'Helpers', 'Http', 'Listeners', 'Providers', 'Traits'])) {
@@ -87,18 +97,23 @@ class UpgradeCommand extends Command
                 (new Filesystem)->copyDirectory($upDir, database_path($subDir));
             }
         }
+        //Step 5
         $this->line('step5: clear file cache');
         InstallHelper::freshSystem();
+        //Step 6
         $this->line('step6: run database migrate');
-        //Artisan::call('migrate', ['--force' => true]);
+        Artisan::call('migrate', ['--force' => true]);
+        //Step 7
         $this->line('step7: run database seed');
         Artisan::call('db:seed', ['--force' => true, '--class'=>'Database\Seeders\UpgradeSeeder']);
+        //Step 8
         $this->line('step8: run program script');
         $upClass = '\\App\\Http\\UpgradeController';
         if (class_exists($upClass)) {
             (new $upClass)->init();
         }
-        $this->info('upgrade end');
+        //Done
+        $this->info('Fresns Upgrade Done');
 
         return Command::SUCCESS;
     }
