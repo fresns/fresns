c8627a84374e6a890392764d180afc5a7119a402
Jarvis Tang <jarvis-tang@users.noreply.github.com>
2021-11-13 13:14:25 +0000

    Apply fixes from StyleCI

--- app/Http/FresnsInstall/FsControllerWeb.php
+++ app/Http/FresnsInstall/FsControllerWeb.php
@@ -104,7 +104,7 @@ class FsControllerWeb
     // register manager
     public function initManage(Request $request)
     {
-        $backend_host = rtrim($request->input('backend_host'),'/');
+        $backend_host = rtrim($request->input('backend_host'), '/');
         $email = $request->input('email');
         $pure_phone = $request->input('pure_phone');
         $country_code = $request->input('country_code');
