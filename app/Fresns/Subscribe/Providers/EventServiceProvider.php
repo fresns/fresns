bb3484c4bcf2bf2f38c91445011510dffd4de6de
Jarvis <jarvis.okay@gmail.com>
2022-03-26 16:24:30 +0800

    style(subscribe): delete useless use

--- app/Fresns/Subscribe/Providers/EventServiceProvider.php
+++ app/Fresns/Subscribe/Providers/EventServiceProvider.php
@@ -9,10 +9,7 @@
 namespace App\Fresns\Subscribe\Providers;
 
 use App\Fresns\Subscribe\SubscribeManager;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
