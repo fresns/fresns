32b9e1d6cb9202f511920252a0dca9ee095eef15
Jarvis <jarvis.okay@gmail.com>
2022-04-21 20:10:25 +0800

    chore(queue): modify config

--- config/queue.php
+++ config/queue.php
@@ -37,7 +37,7 @@ return [
         'database' => [
             'driver' => 'database',
             'table' => 'jobs',
-            'queue' => 'default',
+            'queue' => 'fresns',
             'retry_after' => 90,
             'after_commit' => false,
         ],
@@ -45,7 +45,7 @@ return [
         'beanstalkd' => [
             'driver' => 'beanstalkd',
             'host' => 'localhost',
-            'queue' => 'default',
+            'queue' => 'fresns',
             'retry_after' => 90,
             'block_for' => 0,
             'after_commit' => false,
@@ -56,7 +56,7 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
-            'queue' => env('SQS_QUEUE', 'default'),
+            'queue' => env('SQS_QUEUE', 'fresns'),
             'suffix' => env('SQS_SUFFIX'),
             'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
             'after_commit' => false,
@@ -65,7 +65,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
-            'queue' => env('REDIS_QUEUE', 'default'),
+            'queue' => env('REDIS_QUEUE', 'fresns'),
             'retry_after' => 90,
             'block_for' => null,
             'after_commit' => false,
