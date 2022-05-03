f320e12bd76b033ec5b4ab77c1c0c490cc13c4fd
Jarvis <jarvis.okay@gmail.com>
2022-05-04 04:35:05 +0800

    chore(install): remove use

--- app/Fresns/Install/Routes/api.php
+++ app/Fresns/Install/Routes/api.php
@@ -7,7 +7,6 @@
  */
 
 use App\Fresns\Install\Http\Controllers as ApiController;
-use App\Fresns\Install\Http\Middleware\AppKeyMiddleware;
 use App\Fresns\Install\Http\Middleware\ChangeLanguage;
 use Illuminate\Support\Facades\Route;
 
