f71f9d68ac2834b17ff83f4e854c63fab38a70de
Jarvis <jarvis.okay@gmail.com>
2022-10-18 00:09:08 +0800

    chore(config): log days

--- config/logging.php
+++ config/logging.php
@@ -67,7 +67,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => 3,
         ],
 
         'slack' => [
