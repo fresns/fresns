8ea767491f142ccad6a8b67ec467b6eab86300f7
Jarvis <jarvis.okay@gmail.com>
2022-10-31 01:42:27 +0800

    chore(data): notifies table

--- database/migrations/2022_10_18_090900_create_notifies_table.php
+++ database/migrations/2022_10_18_090900_create_notifies_table.php
@@ -21,16 +21,18 @@ class CreateNotifiesTable extends Migration
     {
         Schema::create('notifies', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id');
             $table->unsignedTinyInteger('type');
+            $table->unsignedBigInteger('user_id');
             $table->text('content')->nullable();
             $table->unsignedTinyInteger('is_markdown')->default(0);
-            $table->string('plugin_unikey', 64)->nullable();
+            $table->unsignedTinyInteger('is_multilingual')->default(0);
             $table->unsignedTinyInteger('is_access_plugin')->default(0);
+            $table->string('plugin_unikey', 64)->nullable();
             $table->unsignedBigInteger('action_user_id')->nullable();
             $table->unsignedSmallInteger('action_type')->nullable();
             $table->unsignedTinyInteger('action_object')->nullable();
             $table->unsignedBigInteger('action_id')->nullable();
+            $table->unsignedBigInteger('action_comment_id')->nullable();
             $table->unsignedTinyInteger('is_read')->default(0);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->nullable();
