46bb65e8e3e8e508f6098c2e02ca067b95d669e9
Jarvis <jarvis.okay@gmail.com>
2023-01-14 00:57:07 +0800

    style(console): remove use

--- app/Console/Commands/Upgrade/Upgrade7Command.php
+++ app/Console/Commands/Upgrade/Upgrade7Command.php
@@ -10,7 +10,6 @@ namespace App\Console\Commands\Upgrade;
 
 use App\Models\Config;
 use App\Models\Language;
-use App\Utilities\ArrUtility;
 use Illuminate\Console\Command;
 use Symfony\Component\Process\Process;
 
