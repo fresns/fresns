999cb1ee9ae51acab2f1130d9048820908751c98
Jarvis <jarvis.okay@gmail.com>
2023-01-20 15:04:14 +0800

    feat(console): fresns:upgrade-12

---
+++ app/Console/Commands/Upgrade/Upgrade12Command.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * Fresns (https://fresns.org)
+ * Copyright (C) 2021-Present Jarvis Tang
+ * Released under the Apache-2.0 License.
+ */
+
+namespace App\Console\Commands\Upgrade;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class Upgrade12Command extends Command
+{
+    protected $signature = 'fresns:upgrade-12';
+
+    protected $description = 'upgrade to fresns v2.2.0';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    // execute the console command
+    public function handle()
+    {
+        logger('upgrade:fresns-12 composerInstall');
+        $this->composerInstall();
+
+        logger('upgrade:fresns-12 migrate');
+        $this->call('migrate', ['--force' => true]);
+
+        return Command::SUCCESS;
+    }
+
+    // composer install
+    public function composerInstall()
+    {
+        $composerPath = 'composer';
+
+        if (! $this->commandExists($composerPath)) {
+            $composerPath = '/usr/bin/composer';
+        }
+
+        $process = new Process([$composerPath, 'install'], base_path());
+        $process->setTimeout(0);
+        $process->start();
+
+        foreach ($process as $type => $data) {
+            if ($process::OUT === $type) {
+                $this->info("\nRead from stdout: ".$data);
+            } else { // $process::ERR === $type
+                $this->info("\nRead from stderr: ".$data);
+            }
+        }
+    }
+
+    // check composer
+    public function commandExists($commandName)
+    {
+        ob_start();
+        passthru("command -v $commandName", $code);
+        ob_end_clean();
+
+        return (0 === $code) ? true : false;
+    }
+}
