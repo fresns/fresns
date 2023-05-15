154f20015ca4bc956654082eb8234757115fbc16
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-05-15 19:26:00 +0800

    chore(api): headers cmd word

--- app/Fresns/Api/Http/Middleware/CheckHeaderByBlacklist.php
+++ app/Fresns/Api/Http/Middleware/CheckHeaderByBlacklist.php
@@ -9,8 +9,6 @@
 namespace App\Fresns\Api\Http\Middleware;
 
 use App\Exceptions\ApiException;
-use App\Fresns\Api\Http\DTO\HeadersDTO;
-use App\Fresns\Words\Basic\DTO\DeviceInfoDTO;
 use App\Helpers\ConfigHelper;
 use App\Utilities\SubscribeUtility;
 use Closure;
@@ -20,60 +18,28 @@ class CheckHeaderByBlacklist
 {
     public function handle(Request $request, Closure $next)
     {
-        $headers = [
-            'appId' => \request()->header('X-Fresns-App-Id'),
-            'platformId' => \request()->header('X-Fresns-Client-Platform-Id'),
-            'version' => \request()->header('X-Fresns-Client-Version'),
-            'deviceInfo' => \request()->header('X-Fresns-Client-Device-Info'),
-            'langTag' => \request()->header('X-Fresns-Client-Lang-Tag'),
-            'timezone' => \request()->header('X-Fresns-Client-Timezone'),
-            'contentFormat' => \request()->header('X-Fresns-Client-Content-Format'),
-            'aid' => \request()->header('X-Fresns-Aid'),
-            'aidToken' => \request()->header('X-Fresns-Aid-Token'),
-            'uid' => \request()->header('X-Fresns-Uid'),
-            'uidToken' => \request()->header('X-Fresns-Uid-Token'),
-            'signature' => \request()->header('X-Fresns-Signature'),
-            'timestamp' => \request()->header('X-Fresns-Signature-Timestamp'),
-        ];
+        $fresnsResp = \FresnsCmdWord::plugin('Fresns')->checkHeaders();
 
-        // check header
-        new HeadersDTO($headers);
-
-        try {
-            $deviceInfo = json_decode($headers['deviceInfo'], true);
-        } catch (\Exception $e) {
-            $deviceInfo = [];
+        if ($fresnsResp->isErrorResponse()) {
+            return $fresnsResp->errorResponse();
         }
-
-        // check deviceInfo
-        new DeviceInfoDTO($deviceInfo);
 
         // current route name
         $currentRouteName = \request()->route()->getName();
 
-        // check sign
-        $isCheckSign = ConfigHelper::fresnsConfigDeveloperMode()['apiSignature'];
-        if ($isCheckSign) {
-            $fresnsResp = \FresnsCmdWord::plugin('Fresns')->verifySign($headers);
-
-            if ($fresnsResp->isErrorResponse()) {
-                return $fresnsResp->errorResponse();
-            }
-        }
-
         // notify user activity
-        if ($headers['uid']) {
+        if ($fresnsResp->getData('uid')) {
             $uri = sprintf('/%s', ltrim(\request()->getRequestUri(), '/'));
 
-            SubscribeUtility::notifyUserActivity($currentRouteName, $uri, $headers, \request()->all());
+            SubscribeUtility::notifyUserActivity($currentRouteName, $uri, $fresnsResp->getData(), \request()->all());
         }
 
         // config
         $siteMode = ConfigHelper::fresnsConfigByItemKey('site_mode');
 
         // account and user login
-        $accountLogin = $headers['aid'] ? true : false;
-        $userLogin = $headers['uid'] ? true : false;
+        $accountLogin = $fresnsResp->getData('aid') ? true : false;
+        $userLogin = $fresnsResp->getData('uid') ? true : false;
 
         // account and user login
         if ($accountLogin && $userLogin) {
