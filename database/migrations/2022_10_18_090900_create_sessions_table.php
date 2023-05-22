887468ea5bf04a712f1d1a31327dbd50b0135a57
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-05-22 13:57:55 +0800

    chore(data): migrations

--- database/migrations/2022_10_18_090900_create_sessions_table.php
+++ database/migrations/2022_10_18_090900_create_sessions_table.php
@@ -10,13 +10,49 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSessionLogsTable extends Migration
+class CreateSessionsTable extends Migration
 {
     /**
      * Run fresns migrations.
      */
     public function up(): void
     {
+        Schema::create('session_keys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedTinyInteger('platform_id');
+            $table->string('name', 64);
+            $table->unsignedTinyInteger('type')->default(1);
+            $table->string('plugin_fskey', 32)->nullable();
+            $table->string('app_id', 8)->unique('app_id');
+            $table->string('app_secret', 32);
+            $table->unsignedTinyInteger('is_read_only')->default(0);
+            $table->unsignedTinyInteger('is_enabled')->default(1);
+            $table->text('remark')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+            $table->softDeletes();
+        });
+
+        Schema::create('session_tokens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedTinyInteger('platform_id');
+            $table->string('version', 16);
+            $table->string('app_id', 8)->nullable()->index('token_app_id');
+            $table->unsignedBigInteger('account_id');
+            $table->string('account_token', 32);
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('user_token', 32)->nullable();
+            $table->string('scope', 128)->nullable();
+            $table->text('payload')->nullable();
+            $table->timestamp('expired_at')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+            $table->softDeletes();
+
+            $table->index(['account_id', 'account_token'], 'account_id_token');
+            $table->unique(['user_id', 'user_token'], 'user_id_token');
+        });
+
         Schema::create('session_logs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('plugin_fskey', 64)->default('Fresns')->index('log_plugin_fskey');
@@ -58,6 +94,8 @@ class CreateSessionLogsTable extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('session_keys');
+        Schema::dropIfExists('session_tokens');
         Schema::dropIfExists('session_logs');
     }
 }
