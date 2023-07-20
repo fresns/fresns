376944a79299c39b5f52bf7601fef42f3fb28aab
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-07-21 00:44:36 +0800

    refactor(data): messages table

--- database/migrations/2022_10_18_090900_create_messages_table.php
+++ database/migrations/2022_10_18_090900_create_messages_table.php
@@ -10,13 +10,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateConversationsTable extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run fresns migrations.
      */
     public function up(): void
     {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedTinyInteger('type');
+            $table->unsignedBigInteger('user_id');
+            $table->text('content')->nullable();
+            $table->unsignedTinyInteger('is_markdown')->default(0);
+            $table->unsignedTinyInteger('is_multilingual')->default(0);
+            $table->unsignedTinyInteger('is_mention')->default(0);
+            $table->unsignedTinyInteger('is_access_plugin')->default(0);
+            $table->string('plugin_fskey', 64)->nullable();
+            $table->unsignedBigInteger('action_user_id')->nullable();
+            $table->unsignedTinyInteger('action_is_anonymous')->default(0);
+            $table->unsignedSmallInteger('action_type')->nullable();
+            $table->unsignedTinyInteger('action_object')->nullable();
+            $table->unsignedBigInteger('action_id')->nullable();
+            $table->unsignedBigInteger('action_content_id')->nullable();
+            $table->unsignedTinyInteger('is_read')->default(0);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+            $table->softDeletes();
+        });
+
         Schema::create('conversations', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('a_user_id');
@@ -54,6 +76,7 @@ class CreateConversationsTable extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('notifications');
         Schema::dropIfExists('conversations');
         Schema::dropIfExists('conversation_messages');
     }
