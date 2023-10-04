578e63d81a310c844a1319b9a7ea5deaa642166c
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-10-05 01:25:37 +0800

    chore(data): plugins

--- database/migrations/2022_10_18_090900_create_plugins_table.php
+++ database/migrations/2022_10_18_090900_create_plugins_table.php
@@ -41,7 +41,7 @@ class CreatePluginsTable extends Migration
             $table->string('plugin_host', 128)->nullable();
             $table->string('access_path')->nullable();
             $table->string('settings_path', 128)->nullable();
-            $table->unsignedTinyInteger('theme_functions')->default(0);
+            $table->unsignedTinyInteger('is_standalone')->default(0);
             $table->unsignedTinyInteger('is_upgrade')->default(0);
             $table->string('upgrade_code', 32)->nullable();
             $table->string('upgrade_version', 16)->nullable();
