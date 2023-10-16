474513f2f3368f3d8946fabe122c1bd24e2cee93
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-10-16 10:42:18 +0800

    chore(http): encrypt cookies

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -20,40 +20,5 @@ class EncryptCookies extends Middleware
     protected $except = [
         'install_lang',
         'panel_lang',
-
-        // website engine
-        'fresns_ulid',
-        'fresns_lang_tag',
-        'fresns_aid',
-        'fresns_aid_token',
-        'fresns_uid',
-        'fresns_uid_token',
-
-        // website one engine
-        'fresns_one_ulid',
-        'fresns_one_lang_tag',
-        'fresns_one_aid',
-        'fresns_one_aid_token',
-        'fresns_one_uid',
-        'fresns_one_uid_token',
-
-        // website two engine
-        'fresns_two_ulid',
-        'fresns_two_lang_tag',
-        'fresns_two_aid',
-        'fresns_two_aid_token',
-        'fresns_two_uid',
-        'fresns_two_uid_token',
-
-        // website three engine
-        'fresns_three_ulid',
-        'fresns_three_lang_tag',
-        'fresns_three_aid',
-        'fresns_three_aid_token',
-        'fresns_three_uid',
-        'fresns_three_uid_token',
-
-        // website editor
-        'fresns_draft',
     ];
 }
