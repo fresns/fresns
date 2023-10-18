b1e95405edb868a826d6ffba9d57552d6e42622d
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-10-18 16:10:13 +0800

    chore(http): encrypt cookies

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -8,7 +8,9 @@
 
 namespace App\Http\Middleware;
 
+use App\Helpers\ConfigHelper;
 use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
+use Illuminate\Contracts\Encryption\Encrypter;
 
 class EncryptCookies extends Middleware
 {
@@ -21,4 +23,20 @@ class EncryptCookies extends Middleware
         'install_lang',
         'panel_lang',
     ];
+
+    public function __construct(Encrypter $encrypter)
+    {
+        parent::__construct($encrypter);
+
+        $cookiePrefix = ConfigHelper::fresnsConfigByItemKey('website_cookie_prefix');
+
+        $this->except = array_merge($this->except, [
+            "{$cookiePrefix}ulid",
+            "{$cookiePrefix}lang_tag",
+            "{$cookiePrefix}aid",
+            "{$cookiePrefix}aid_token",
+            "{$cookiePrefix}uid",
+            "{$cookiePrefix}uid_token",
+        ]);
+    }
 }
