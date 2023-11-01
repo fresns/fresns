05fcb21afa413135f4a3477d2ee9af8f1f9f261f
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-11-01 17:45:41 +0800

    chore(panel): group paginate

--- app/Fresns/Panel/Http/Controllers/GroupController.php
+++ app/Fresns/Panel/Http/Controllers/GroupController.php
@@ -59,7 +59,7 @@ class GroupController extends Controller
                 ->where('parent_id', $parentId)
                 ->isEnabled()
                 ->with('creator', 'followByPlugin', 'names', 'descriptions', 'admins')
-                ->get();
+                ->paginate(25);
         }
 
         extract(get_object_vars($this));
@@ -105,7 +105,7 @@ class GroupController extends Controller
             ->with('creator', 'followByPlugin', 'category', 'admins')
             ->where('is_recommend', 1)
             ->isEnabled()
-            ->get();
+            ->paginate(25);
 
         $plugins = Plugin::all();
         $plugins = $plugins->filter(function ($plugin) {
@@ -138,7 +138,7 @@ class GroupController extends Controller
             ->where('is_enabled', false)
             ->orderBy('rating')
             ->with('creator', 'followByPlugin', 'category')
-            ->get();
+            ->paginate(25);
 
         $plugins = Plugin::all();
         $plugins = $plugins->filter(function ($plugin) {
