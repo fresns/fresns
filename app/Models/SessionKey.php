87b608f19141b090decdfafb5b148daaead0333d
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-12-28 21:03:01 +0800

    feat(models): platform const

--- app/Models/SessionKey.php
+++ app/Models/SessionKey.php
@@ -16,6 +16,18 @@ class SessionKey extends Model
     const TYPE_MANAGE = 2;
     const TYPE_APP = 3;
 
+    const PLATFORM_OTHER = 1;
+    const PLATFORM_WEB_DESKTOP = 2;
+    const PLATFORM_WEB_MOBILE = 3;
+    const PLATFORM_WEB_RESPONSIVE = 4;
+    const PLATFORM_APP_IOS = 5;
+    const PLATFORM_APP_ANDROID = 6;
+    const PLATFORM_WECHAT = 7;
+    const PLATFORM_QQ = 8;
+    const PLATFORM_ALIPAY = 9;
+    const PLATFORM_BYTEDANCE = 10;
+    const PLATFORM_QUICK_APP = 11;
+
     use Traits\IsEnabledTrait;
 
     public function platformName($platforms = []): string
