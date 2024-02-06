797f6d48577419478f47b0e359bea2ccc40c1215
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2024-02-06 08:56:35 +0800

    feat(models): city

---
+++ app/Models/City.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Fresns (https://fresns.org)
+ * Copyright (C) 2021-Present Jevan Tang
+ * Released under the Apache-2.0 License.
+ */
+
+namespace App\Models;
+
+class City extends Model
+{
+    protected $casts = [
+        'continent' => 'json',
+        'country' => 'json',
+        'region' => 'json',
+        'city' => 'json',
+    ];
+}
