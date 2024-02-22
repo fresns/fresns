e178195e6480efdc4d3832457be0e2f44a30adec
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2024-02-23 01:18:16 +0800

    chore(install): route name

--- app/Fresns/Install/Routes/api.php
+++ app/Fresns/Install/Routes/api.php
@@ -10,7 +10,7 @@ use App\Fresns\Install\Http\Controllers as ApiController;
 use App\Fresns\Install\Http\Middleware\ChangeLanguage;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api')->middleware([
+Route::prefix('api')->name('api.')->middleware([
     ChangeLanguage::class,
 ])->group(function () {
     Route::post('install', [ApiController\InstallController::class, 'install'])->name('install.fresns');
