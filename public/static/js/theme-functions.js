446528b68cdcbac01ebf594e326a0b474cf3f4a2
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2024-02-28 14:27:28 +0800

    feat: theme functions js

---
+++ public/static/js/theme-functions.js
@@ -0,0 +1,71 @@
+/*!
+ * Fresns (https://fresns.org)
+ * Copyright 2021-Present Jevan Tang
+ * Licensed under the Apache-2.0 license
+ */
+
+/* Tooltips */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+// copyright-year
+var yearElement = document.querySelector('.copyright-year');
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+if (yearElement) {
+    yearElement.textContent = currentYear;
+}
+
+/* Fresns Token */
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+    }
+});
+
+// set timeout toast hide
+const setTimeoutToastHide = () => {
+    $('.toast.show').each((k, v) => {
+        setTimeout(function () {
+            $(v).hide();
+        }, 1500);
+    });
+};
+setTimeoutToastHide();
+
+// tips
+window.tips = function (message, code = 200) {
+    let html = `<div aria-live="polite" aria-atomic="true" class="position-fixed top-50 start-50 translate-middle" style="z-index:99999">
+        <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <img src="/static/images/icon.png" width="20px" height="20px" class="rounded me-2" alt="Fresns">
+                <strong class="me-auto">Fresns</strong>
+                <small>${code}</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">${message}</div>
+        </div>
+    </div>`;
+    $('div.fresns-tips').prepend(html);
+    setTimeoutToastHide();
+};
+
+// set
+$(document).ready(function () {
+    // select2
+    $('.select2').select2({
+        theme: 'bootstrap-5',
+    });
+
+    $('.select2').closest('form').on('reset', function (ev) {
+        var targetJQForm = $(ev.target);
+        setTimeout(
+            function () {
+                this.find('.select2').trigger('change');
+            }.bind(targetJQForm),
+            0
+        );
+    });
+});
