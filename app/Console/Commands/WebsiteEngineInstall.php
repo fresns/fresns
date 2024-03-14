c1c4988598b907de1f6a2df44d8de445c5038cbe
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2024-03-15 00:27:26 +0800

    feat(console): website engine

---
+++ app/Console/Commands/WebsiteEngineInstall.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * Fresns (https://fresns.org)
+ * Copyright (C) 2021-Present Jevan Tang
+ * Released under the Apache-2.0 License.
+ */
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Fresns\PluginManager\Support\Process;
+
+class WebsiteEngineInstall extends Command
+{
+    protected $signature = 'fresns:install-website';
+
+    protected $description = 'Install fresns website engine';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    // composer require fresns/website-engine
+    public function handle()
+    {
+        $websiteEngineExists = class_exists(\Fresns\WebsiteEngine\Providers\WebsiteEngineServiceProvider::class);
+
+        if ($websiteEngineExists) {
+            $this->info('Website engine already installed');
+
+            return Command::SUCCESS;
+        }
+
+        $httpProxy = config('app.http_proxy');
+
+        $process = Process::run(<<<"SHELL"
+            export http_proxy=$httpProxy https_proxy=$httpProxy
+            echo http_proxy=\$http_proxy
+            echo https_proxy=\$https_proxy
+            echo "current user:" `whoami`
+            echo "home path permission is:" `ls -ld ~`
+            echo ""
+
+            #test -f ~/.config/composer/composer.json && echo 1 || (mkdir -p ~/.config/composer && echo "{}" > ~/.config/composer/composer.json)
+            #echo ""
+
+            echo "global composer.json content": `cat ~/.config/composer/composer.json`
+            echo ""
+
+            echo "PATH:" `echo \$PATH`
+            echo ""
+
+            echo "php:" `which php` "\n version" `php -v`
+            echo "composer:" `which composer` "\n version" `composer --version`
+            echo "git:" `which git` "\n version" `git --version`
+            echo ""
+
+            # install command
+            composer diagnose
+            composer require fresns/website-engine
+        SHELL, $this->output);
+
+        if (! $process->isSuccessful()) {
+            $this->error('Failed to install fresns/website-engine');
+
+            return Command::FAILURE;
+        }
+
+        $this->info('Website Engine Installation Successful');
+
+        return Command::SUCCESS;
+    }
+}
