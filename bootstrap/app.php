d52f39ceced0c534254fc8cef2c6c238987b1c30
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2024-03-15 00:20:49 +0800

    Initial commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,55 +1,35 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
+use App\Helpers\ConfigHelper;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-$app = new Illuminate\Foundation\Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
+// cookie name
+try {
+    $cookiePrefix = ConfigHelper::fresnsConfigByItemKey('website_cookie_prefix') ?? 'fresns_';
+} catch (\Exception $e) {
+    $cookiePrefix = 'fresns_';
+}
 
-/*
-|--------------------------------------------------------------------------
-| Bind Important Interfaces
-|--------------------------------------------------------------------------
-|
-| Next, we need to bind some important interfaces into the container so
-| we will be able to resolve them when needed. The kernels serve the
-| incoming requests to this application from both the web and CLI.
-|
-*/
+$encryptCookies = [
+    'fresns_timezone',
+    "{$cookiePrefix}lang_tag",
+    "{$cookiePrefix}aid",
+    "{$cookiePrefix}aid_token",
+    "{$cookiePrefix}uid",
+    "{$cookiePrefix}uid_token",
+];
 
-$app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
-);
-
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-|
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
-|
-*/
-
-return $app;
+return Application::configure(basePath: dirname(__DIR__))
+    // ->withRouting(
+    //     // web: __DIR__.'/../routes/web.php',
+    //     // commands: __DIR__.'/../routes/console.php',
+    //     // health: '/up',
+    // )
+    ->withMiddleware(function (Middleware $middleware) use ($encryptCookies) {
+        $middleware->encryptCookies(except: $encryptCookies);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
---
+++ bootstrap/providers.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    // Common Service Providers
+    App\Providers\SqlLogServiceProvider::class,
+    App\Providers\MarketServiceProvider::class,
+
+    // Fresns Service Providers
+    App\Fresns\Install\Providers\InstallServiceProvider::class,
+    App\Fresns\Panel\Providers\PanelServiceProvider::class,
+    App\Fresns\Words\Providers\CmdWordServiceProvider::class,
+    App\Fresns\Api\Providers\ApiServiceProvider::class,
+];
