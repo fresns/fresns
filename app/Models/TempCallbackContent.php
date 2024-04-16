c93ecb6bb77530d707e1354d0eb7d43b71302943
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2024-04-16 17:31:07 +0800

    chore(models): callback const

--- app/Models/TempCallbackContent.php
+++ app/Models/TempCallbackContent.php
@@ -14,17 +14,18 @@ class TempCallbackContent extends Model
     const TYPE_AUTHORIZATION = 2;
     const TYPE_TOKEN = 3;
     const TYPE_ACCOUNT = 4;
-    const TYPE_USER = 5;
-    const TYPE_GROUP = 6;
-    const TYPE_HASHTAG = 7;
-    const TYPE_GEOTAG = 8;
-    const TYPE_POST = 9;
-    const TYPE_COMMENT = 10;
-    const TYPE_ARCHIVE = 11;
-    const TYPE_EXTEND = 12;
-    const TYPE_OPERATION = 13;
-    const TYPE_FILE = 14;
-    const TYPE_LOCATION_INFO = 15;
+    const TYPE_WALLET = 5;
+    const TYPE_USER = 6;
+    const TYPE_GROUP = 7;
+    const TYPE_HASHTAG = 8;
+    const TYPE_GEOTAG = 9;
+    const TYPE_POST = 10;
+    const TYPE_COMMENT = 11;
+    const TYPE_ARCHIVE = 12;
+    const TYPE_EXTEND = 13;
+    const TYPE_OPERATION = 14;
+    const TYPE_FILE = 15;
+    const TYPE_LOCATION_INFO = 16;
 
     use Traits\IsEnabledTrait;
 
