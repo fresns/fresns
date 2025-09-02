53c97486f0ac0274e04178896bb9ec56047932b7
Jevan <22316055+jevantang@users.noreply.github.com>
2025-09-03 01:21:31 +0800

    build: v3.8.1

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Str;
 
 class AppHelper
 {
-    const VERSION = '3.8.0';
+    const VERSION = '3.8.1';
 
     // fresns test helper
     public static function fresnsTestHelper(): mixed
